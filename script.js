ad1a0b2eea55e81365c0f3c7832e85081f751f70
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2022-05-26 16:52:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 100){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+});
+

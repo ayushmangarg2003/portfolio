73e55ed66054a227d39821b14bcc87dfdb3fa2f1
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2022-05-28 11:03:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,4 +8,8 @@ $(document).ready(function(){
         }
     });
 });
+$('.menu-btn').click(function(){
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+});
 

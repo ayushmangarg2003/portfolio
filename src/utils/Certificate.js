ffe090486151d6f8a728e44f4d0c832d27dd3646
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2024-04-06 23:13:12 +0530

    Update Certificate.js

--- src/utils/Certificate.js
+++ src/utils/Certificate.js
@@ -3,6 +3,12 @@ import Gymnasium from "./assets/gymnasium.webp"
 import Mait from "./assets/MaitLogo.webp"
 
 const Certificates = [
+     {
+        logo:Ibm,
+        title: "Generative AI for Software Developers Specialization",
+        organisation: "IBM",
+        link:"https://coursera.org/share/cc391d4b518949d92919e8454fd7bc94"
+    },
     {
         logo: Ibm,
         title: "Introduction to Web Development with HTML, CSS and Javascript",
@@ -15,11 +21,6 @@ const Certificates = [
         organisation: "Gymnasium",
         link:"https://www.credential.net/3611f2e5-78ba-4f54-b4b2-c3376dae275f#gs.3iebig"
     },
-    {
-        logo:Mait,
-        title: "HackMait3.0 Hackaton",
-        organisation: "Maharaja Agrasen Institute of Technology",
-        link:"https://drive.google.com/file/d/12IPtkkI22CUBlWMkokg2LZOfFcv26a7H/view"
-    },
+   
 ]
 export default Certificates;

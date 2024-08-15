a7d74afb1ec549761c1fbc6bc7db48e722ff22d1
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2024-08-15 18:11:01 +0530

    Update Helper.js

--- src/utils/Helper.js
+++ src/utils/Helper.js
@@ -8,7 +8,7 @@ const LINKEDIN = "https://www.linkedin.com/in/ayushmangarg/";
 const TWITTER = "https://twitter.com/AyushmanGarg4";
 const GITHUB = "https://github.com/ayushmangarg2003";
 const MAIL = "https://mail.google.com/mail/u/0/?fs=1&to=ayushmangarg929@gmail.com&tf=cm";
-const CV = "https://drive.google.com/file/d/1eAOPy_5eZWfNjxCGRaNhwcgpwvGcCR8B/view";
+const CV = "https://drive.google.com/file/d/1eAOPy_5eZWfNjxCGRaNhwcgpwvGcCR8B/view?usp=sharing";
 const DETAILS_1 = "Hi, my name is Ayushman Garg, I am passionate about MERN stack web development, React Native, and exploring generative AI for crafting intuitive user experiences. I am committed to continuous learning and contributing positively to software development projects, seeking opportunities to gain hands-on experience and grow in the field."
 const DETAILS_2 = "My programming journey started with Python in school and evolved during my first year of college into a passion for web development, and then into Machine Learning. Since then, I have gained experience working with technologies like HTML, CSS, JavaScript, ReactJS, MongoDB, ExpressJS, NodeJS, MySQL, React Native, Dialogflow and various other frameworks."
 

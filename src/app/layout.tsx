68ceaf37f07bb34d8315b715c4266e7bfd76ef07
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2025-01-18 16:42:06 +0530

    GTAG Added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -58,6 +59,7 @@ export default function RootLayout({
       <head>
         <base target="_blank"></base>
       </head>
+
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
@@ -71,6 +73,7 @@ export default function RootLayout({
           </TooltipProvider>
         </ThemeProvider>
       </body>
+      <GoogleAnalytics gaId="G-NEH8JMSSQJ" />
     </html>
   );
 }
